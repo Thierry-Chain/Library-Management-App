feba8dda8e5ad8e6c7c1f277ee1a77e262f18fb8
Iramboana-Thierry <irambonat0@gmail.com>
2020-10-23 10:55:53 +0200

    after destructuring token fro redux store

--- src/redux/authHeader.js
+++ src/redux/authHeader.js
@@ -1,8 +1,9 @@
 import store from './store'
 let allData=store.getState()
-const token=allData.user.more.token
+const {token}=allData.user.more
 const authHeader={
     'Content-Type':'application/json',  
  'auth-token':`${token}`
 }
+console.log('auth-header with store',authHeader)
 export default  authHeader
