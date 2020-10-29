3dfaa897f04df1e4b9a91131baaad690b89e248a
Iramboana-Thierry <irambonat0@gmail.com>
2020-10-29 23:58:30 +0200

    hiden credentials

--- src/locations.js
+++ src/locations.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
-export const location='http://localhost:5000/api'
-export const headers={
-    'Content-Type':'application/json'
+export const location = 'null'
+export const headers = {
+    'Content-Type': 'application/json'
 }
-const token=localStorage.getItem('token')
-export const authHeader={
-    'Content-Type':'application/json',  
- 'auth-token':`${JSON.parse(token)}`
+const token = localStorage.getItem('token')
+export const authHeader = {
+    'Content-Type': 'application/json',
+    'auth-token': `${JSON.parse(token)}`
 }
