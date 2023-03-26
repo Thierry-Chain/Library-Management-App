dfbc5fada3bee46075057a8ef3e2d227a4a3ca4e
Thierry-Chain <irambonat0@gmail.com>
2023-03-26 18:50:54 +0200

    stored backend link in environment variables instead of from file

--- src/locations.js
+++ src/locations.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
-export const location = 'https://manage-library-api.onrender.com/user/api'; //backend links
+export const location = process.env.REACT_APP_PROD_BACKEND_URL //backend links
+
 export const headers = {
   'Content-Type': 'application/json',
-};
-const token = localStorage.getItem('token');
+}
+const token = localStorage.getItem('token')
 export const authHeader = {
   'Content-Type': 'application/json',
   'auth-token': `${JSON.parse(token)}`,
-};
+}
